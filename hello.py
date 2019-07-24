f02ef18353e318b047160182ed19df4723840b1d
livs4ever <51410489+livs4ever@users.noreply.github.com>
2019-07-24 23:19:50 +0300

    calculator v5

--- hello.py
+++ hello.py
@@ -2,25 +2,25 @@
 
 # Python Program - Make Simple Calculator
 
-print("1. Addition");
-print("2. Subtraction");
-print("3. Multiplication");
-print("4. Division");
-print("5. Exit");
-choice = int(input("Enter your choice: "));
-if (choice>=1 and choice<=4):
-    print("Enter two numbers: ");
-    num1 = int(input());
-    num2 = int(input());
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Exit")
+choice = int(input("Enter your choice: "))
+if (choice >= 1 and choice <= 4):
+    print("Enter two numbers: ")
+    num1 = int(input())
+    num2 = int(input())
     if choice == 1:
-    	res = num1 + num2;
-    	print("Result = ", res);
+    	res = num1 + num2
+    	print("Result = ", res)
     elif choice == 2:
     	res = num1 - num2;
-    	print("Result = ", res);
+    	print("Result = ", res)
     elif choice == 3:
     	res = num1 * num2;
-    	print("Result = ", res);
+    	print("Result = ", res)
     else:
     	res = num1 / num2;
     	print("Result = ", res)
