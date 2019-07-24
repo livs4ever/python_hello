4bf79c5c7e91c6be45ab158ddcaaa596dc91d42c
livs4ever <51410489+livs4ever@users.noreply.github.com>
2019-07-24 23:38:02 +0300

    calculator v9

--- hello.py
+++ hello.py
@@ -7,24 +7,24 @@ print "2. Subtraction"
 print "3. Multiplication"
 print "4. Division"
 print "5. Exit"
-choice = int(input("Enter your choice: "))
-if (choice >= 1 and choice <= 4):
+CHOICE = int(input("Enter your choice: "))
+if (CHOICE >= 1 and CHOICE <= 4):
     print "Enter two numbers: "
-    num1 = int(input())
-    num2 = int(input())
-    if choice == 1:
-        res = num1 + num2
+    NUM1 = int(input())
+    NUM2 = int(input())
+    if CHOICE == 1:
+        RES = NUM1 + NUM2
 	print("Result = ", res)
-    elif choice == 2:
-	res = num1 - num2
-	print("Result = ", res)
-    elif choice == 3:
-	res = num1 * num2
-	print("Result = ", res)
+    elif CHOICE == 2:
+	RES = NUM1 - NUM2
+	print("Result = ", RES)
+    elif CHOICE == 3:
+	RES = NUM1 * NUM2
+	print("Result = ", RES)
     else:
-	res = num1 / num2
-    	print("Result = ", res)
-elif choice == 5:
+	RES = NUM1 / NUM2
+	print("Result = ", RES)
+elif CHOICE == 5:
     exit()
 else:
     print "Wrong input..!!"
