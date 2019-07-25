28b3fff130f244d3e7b46c9d519fe5fe452b0d1f
livs4ever <51410489+livs4ever@users.noreply.github.com>
2019-07-25 11:46:37 +0300

    v15

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python
- """This script prompts a user to enter a message to encode or decode
- using a classic Caeser shift substitution (3 letter shift)"""
  import string
  print "Hello World"
